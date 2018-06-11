4dc078d05c2ba37832c3c7baabc31ff465d721a2
chenruijia <chenruijia@kingsoft.com>
2018-06-11 20:52:56 +0800

    完成21合并两个链表

---
+++ 21_Merge_Two_Sorted_Lists.go
@@ -0,0 +1,48 @@
+package main
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil && l2 == nil {
+		return nil
+	}
+
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	var head, node *ListNode
+	// 先找出头结点
+	if l1.Val < l2.Val {
+		head = l1
+		node = l1
+		l1 = l1.Next
+	} else {
+		head = l2
+		node = l2
+		l2 = l2.Next
+	}
+
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			node.Next = l1
+			l1 = l1.Next
+		} else {
+			node.Next = l2
+			l2 = l2.Next
+		}
+		node = node.Next
+	}
+
+	if l1 != nil {
+		node.Next = l1
+	}
+
+	if l2 != nil {
+		node.Next = l2
+	}
+
+	return head
+}
