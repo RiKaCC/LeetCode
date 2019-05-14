dce55d36d6ebfb4068c58aac5d72a677b606f29b
chenruijia <chenruijia@kingsoft.com>
2019-05-14 16:32:44 +0800

    add new

--- 39_Combination_Sum.go
+++ 39_Combination_Sum.go
@@ -2,8 +2,44 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func combinationSum(candidates []int, target int) [][]int {
+	// 先对数组进行排序
+	sort.Ints(candidates)
+	current := []int{}
+	res := [][]int{}
 
+	dfs(candidates, target, 0, current, &res)
+	return res
+}
+
+func dfs(candidates []int, target int, index int, current []int, res *[][]int) {
+	if target == 0 {
+		*res = append(*res, current)
+		return
+	}
+
+	for i := index; i < len(candidates); i++ {
+		// 做一个剪枝判断，减少不必要的递归
+		if candidates[i] > target {
+			break
+		}
+
+		// 将当前的数放入current中
+		tmp := current
+		current = append(current, candidates[i])
+		dfs(candidates, target-candidates[i], i, current, res)
+		// current = [2, 2, 2], tmp = [2, 2], 这里就是将candidates[i]弹出，因为candidates[i]已经用完了
+		current = tmp
+	}
+
+	return
+}
+
+func main() {
+	candidates := []int{1, 2, 6, 3, 7}
+	target := 13
+	fmt.Println(combinationSum(candidates, target))
 }
--- 88_Merge_Sorted_Array.go
+++ 88_Merge_Sorted_Array.go
@@ -4,12 +4,44 @@ import (
 	"fmt"
 )
 
-func merge(nums1 []int, m int, nums2 []int, n int)  {
-	i, j := 0
-
-	for i < m {
-		for j < n {
-			if nums2[j] > nums[i]
-		}
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	// 将nums1所有元素向后移n（nums2的长度）位
+	for i := m - 1; i >= 0; i-- {
+		nums1[i+n] = nums1[i]
 	}
+
+	i := n
+	j := 0
+	k := 0
+
+	for i < m+n && j < n {
+		if nums1[i] < nums2[j] {
+			nums1[k] = nums1[i]
+			i++
+		} else {
+			nums1[k] = nums2[j]
+			j++
+		}
+		k++
+	}
+
+	for i < m+n {
+		nums1[k] = nums1[i]
+		i++
+		k++
+	}
+
+	for j < n {
+		nums1[k] = nums2[j]
+		j++
+		k++
+	}
+
+	fmt.Println(nums1)
+}
+
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	merge(nums1, 3, nums2, 3)
 }
